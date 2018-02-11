0294ef616fcaa546307de472aa1a02de32082cc2
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-02-11 11:58:15 +0100

    v0.0.2
    JAVASCRIPT
    + Added cars and player's car drawing
    + Added car "falling"
    + Modified borders detection to prevent player from moving out of canvas
    + Added crash detection

--- game.js
+++ game.js
@@ -2,21 +2,39 @@ document.addEventListener("DOMContentLoaded", function(){
     canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext("2d");
     document.addEventListener("keydown", keyPush);
-    setInterval(game, 1000/10);
+    setInterval(game, 1000/30);
 });
-
+//game
 gs = 100;
-tx=3; ty=9;
-
+tx=5; ty=9;
+//player
 px=2; py=7;
 
+//car
+cx=2; cy=0;
+gravity = 0.3;
 function game() {
-    3
+    
     ctx.fillStyle = "black";
     ctx.fillRect(0,0,canvas.width,canvas.height);
     
+    cy += gravity;
+    
+    if((cy*gs)+180 >= py*gs && cx == px){
+        px=2; cx=2; cy=0;
+    }
+    
+    if(cy > ty){
+        cy=0;
+        cx = Math.floor(Math.random()*5)
+    }
+    
     ctx.fillStyle = "dodgerblue";
     ctx.fillRect(px*gs,py*gs,gs-2,(2*gs)-2);
+    
+    ctx.fillStyle = "red";
+    ctx.fillRect(cx*gs,cy*gs,gs-2,(2*gs)-2);
+    
 }
 
 function keyPush(evt) {
@@ -30,9 +48,9 @@ function keyPush(evt) {
     }
     
     if(px >= tx){
-        px=0;
+        px=tx-1;
     }
     if(px < 0){
-        px=tx-1;
+        px=0;
     }
 }
